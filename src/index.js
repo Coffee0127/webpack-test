59d27e110b9969b7cfb7eac72d00da32f0e5435d
Bo-Xuan Fan <coffee0127@gmail.com>
2017-01-27 14:37:09 +0800

    加入 css-loader & style-loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// 因為有設定 webpack loader，故可以 require css 檔案
+// require('./styles.css');
+
 console.log('Hello webpack!!');
 
 function gretting(msg) {
--- webpack.config.js
+++ webpack.config.js
@@ -23,5 +23,17 @@ module.exports = {
     // https://webpack.github.io/docs/list-of-plugins.html#occurrenceorderplugin
     // 根據調用次數指定 module 和 chunk 的 id。越常用的 id 較小（短）的 id。這使得 id 可以預測，可以減少檔案的大小
     new webpack.optimize.OccurrenceOrderPlugin()
-  ]
+  ],
+
+  // 設定檔案選項
+  module: {
+    // 設定 loader 陣列
+    loaders: [{
+      // 使用正則表示式，找出要套用 loader 的檔案
+      test: /\.css$/,
+      // 指定哪些 loader 適用於匹配前述 test 檔案
+      // 被指定的 loader 是有順序的，依序 css -> styles
+      loaders: [ 'style', 'css' ]
+    }]
+  }
 };

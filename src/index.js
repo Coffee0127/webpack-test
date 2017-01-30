9287c4f3772f43791e3557fb6a20c3d8421a0972
Bo-Xuan Fan <coffee0127@gmail.com>
2017-01-30 22:31:06 +0800

    使用 webpack hot module replacement

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 // 因為有設定 webpack loader，故可以 require css 檔案
 // require('./styles.css');
 
-console.log('Hello webpack!!');
-
-function gretting(msg) {
-  if (msg) {
-    console.log(msg);
-  } else {
-    console.log('No message input!!');
-  }
+// 使 Hot Module Replacement 可以執行
+if (module.hot) {
+  module.hot.accept();
 }
+
+require('./styles.css');  // 載入網頁樣式
+
+var Please = require('pleasejs');
+var div = document.getElementById('color');
+var button = document.getElementById('button');
+
+function changeColor() {
+  div.style.backgroundColor = Please.make_color();
+}
+
+button.addEventListener('click', changeColor);
